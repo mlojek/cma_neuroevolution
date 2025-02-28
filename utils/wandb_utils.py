0bcd541516822a692babfd1aad7b3084845e0296
mlojek <mlojek@protonmail.com>
2025-02-28 16:29:55 +0100

    Minor improvements to wandb code

--- utils/wandb_utils.py
+++ utils/wandb_utils.py
@@ -4,13 +4,15 @@ https://docs.wandb.ai/quickstart/
 """
 
 import wandb
+from data_model import ExperimentConfig
 
 
-def init_wandb(run_name, optimizer_name):
+def init_wandb(run_name, config: ExperimentConfig):
     wandb.init(
-        project="cma-es-vs-adam", name=run_name, config={"optimizer": optimizer_name}
+        project="cma_neuroevolution", name=run_name, config=config
     )
 
 
 def log_metrics(epoch, loss, accuracy):
+    # epoch, [train, test] x [loss, config]
     wandb.log({"epoch": epoch, "loss": loss, "accuracy": accuracy})
