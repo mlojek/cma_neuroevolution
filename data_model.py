09b26e44c6369b76f9d6332847531f19e80bbe5a
mlojek <mlojek@protonmail.com>
2025-02-28 14:06:16 +0100

    Add config dataclass and enums used in it

---
+++ data_model.py
@@ -0,0 +1,46 @@
+"""
+Data classes and enums that hold data.
+"""
+
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class OptimizationType(Enum):
+    """
+    Enum describing the type of optimization strategy.
+    """
+
+    GRADIENT = "gradient"
+    CMAES = "cmaes"
+
+
+class GradientMethod(Enum):
+    """
+    Enum describing gradient optimization method.
+    """
+
+    SGD = "sgd"
+    ADAM = "adam"
+
+
+class CMAMethod(Enum):
+    """
+    Enum describing the type of CMA neuroevolution method.
+    """
+
+    WHOLE_MODEL = "whole_model"
+    HEAD_FIRST = "head_first"
+    HEAD_LAST = "head_last"
+
+
+class ExperimentConfig(BaseModel):
+    """
+    Configuration of an experiment.
+    """
+
+    optimization_type: OptimizationType
+    optimization_method: GradientMethod | CMAMethod
+    epochs: int
+    batch_size: int
