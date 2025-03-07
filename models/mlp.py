ec5f2ef9e6f2fefaa8cfafe328599512ab63ef4d
mlojek <mlojek@protonmail.com>
2025-03-07 10:34:59 +0100

    Change MLP to MLPClassifier

--- models/mlp.py
+++ models/mlp.py
@@ -1,13 +1,14 @@
 """
-Simple multi-layer perceptron model. Default layer dimensionality matches the Iris dataset.
+Simple multi-layer perceptron classifier model.
+Default layer dimensionality matches the Iris dataset.
 """
 
 from torch import Tensor, nn
 
 
-class MLP(nn.Module):
+class MLPClassifier(nn.Module):
     """
-    Simple multi-layer perceptron model.
+    Simple multi-layer perceptron classifier model.
     """
 
     def __init__(self, input_dim=4, hidden_dim=10, output_dim=3):
@@ -20,9 +21,10 @@ class MLP(nn.Module):
             output_dim (int): Number of outputs.
         """
         super().__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.linear1 = nn.Linear(input_dim, hidden_dim)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.linear2 = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.Softmax()
 
     def forward(self, x: Tensor) -> Tensor:
         """
@@ -34,5 +36,7 @@ class MLP(nn.Module):
         Returns:
             Tensor: Output values.
         """
-        x = self.relu(self.fc1(x))
-        return self.fc2(x)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        return self.softmax(x)
