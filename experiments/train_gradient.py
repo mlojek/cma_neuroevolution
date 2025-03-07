d97ac75a7c3fa10d141d181b85ffacff01e9fae9
mlojek <mlojek@protonmail.com>
2025-03-07 10:35:27 +0100

    Update train_gradient function to do validation each epoch

--- experiments/train_gradient.py
+++ experiments/train_gradient.py
@@ -4,70 +4,118 @@ Training of model with gradient descent methods.
 
 import torch
 from torch import nn, optim
+from torch.utils.data import DataLoader, TensorDataset
 
-from data.iris_loader import load_iris_dataset
-from models.mlp import MLP
+from models.mlp import MLPClassifier
 from utils.wandb_utils import init_wandb, log_metrics
-from data_model import ExperimentConfig, OptimizationType, GradientMethod
 
 
 def train_gradient(
-    config: ExperimentConfig
-) -> nn.Module:
-    assert config.optimization_type == OptimizationType.GRADIENT
-    # TODO loaders passed as argments, remove batch_size
-    # Load data
-    train_loader, test_loader = load_iris_dataset(config.dataset)
+    model: MLPClassifier,
+    train_dataset: TensorDataset,
+    val_dataset: TensorDataset,
+    epochs: int,
+    learning_rate: float,
+    *,
+    batch_size: int = 16,
+    use_wandb: bool = False,
+    optimizer: str = "adam",
+) -> MLPClassifier:
+    """
+    Train the MLP classifier using a gradient optimization method.
 
-    # Initialize model, loss function, and optimizer
-    model = MLP()
-    criterion = nn.CrossEntropyLoss()
+    Args:
+        model (MLPClassifier): The model to train.
+        train_dataset (TensorDataset): Training split of the dataset.
+        val_dataset (TensorDataset): Validation split of the dataset.
+        epochs (int): Number of training epochs.
+        learning_rate (float): Learning rate value for the optimizer.
+        batch_size (int): Number of samples per batch, default 16.
+        use_wandb (bool): If true, loss and accuracy metrics will be logged
+            to wandb.ai, defualt False.
+        optimizer (str): Name of gradient optimizer to use. Available options
+            are sgd, adam. Default is Adam.
 
-    match config.optimization_method:
-        case GradientMethod.ADAM:
-            optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
-        case GradientMethod.SGD:
-            optimizer = optim.SGD(model.parameters(), lr=config.learning_rate)
+    Returns:
+        MLPClassifier: Trained classifier model.
+    """
+    match optimizer:
+        case "adam":
+            optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        case "sgd":
+            optimizer = optim.SGD(model.parameters(), lr=learning_rate)
         case _:
-            raise ValueError(f"Invalid optmizer {config.optimization_method}")
+            raise ValueError(f"Invalid optmizer {optimizer}!")
 
-    # Initialize Weights & Biases
-    if config.logging.use_wandb:
-        init_wandb(run_name="adam-run", optimizer_name="Adam")
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    # Training loop
-    for epoch in range(1, config.epochs + 1):
+    loss_function = nn.CrossEntropyLoss()
+
+    # if use_wandb:
+    #     init_wandb(run_name="adam-run", optimizer_name="Adam")
+
+    for epoch in range(epochs):
+        # Training step
         model.train()
-        total_loss = 0
-        correct = 0
-        total_samples = 0
 
-        for X_batch, y_batch in train_loader:
+        train_loss = 0
+        train_correct_samples = 0
+        train_num_samples = 0
+
+        for x_batch, y_batch in train_loader:
             optimizer.zero_grad()
-            output = model(X_batch)
-            loss = criterion(output, y_batch)
-            loss.backward()
+            y_predicted = model(x_batch)
+            train_loss = loss_function(y_predicted, y_batch)
+            train_loss.backward()
             optimizer.step()
 
-            total_loss += loss.item() * X_batch.size(0)
-            _, predicted = torch.max(output, 1)
-            correct += (predicted == y_batch).sum().item()
-            total_samples += y_batch.size(0)
+            train_loss += train_loss.item() * x_batch.size(0)
 
-        # Compute epoch loss and accuracy
-        avg_loss = total_loss / total_samples
-        accuracy = correct / total_samples
+            predicted_labels = torch.max(y_predicted, 1)[1]
+            train_correct_samples += (predicted_labels == y_batch).sum().item()
+            train_num_samples += y_batch.size(0)
 
-        # TODO evaluation on test set
+        # Compute train loss and accuracy
+        train_avg_loss = train_loss / train_num_samples
+        train_accuracy = train_correct_samples / train_num_samples
 
-        # Log results
-        if config.logging.use_wandb:
-            log_metrics(epoch, avg_loss, accuracy)
+        # Validation step
+        model.eval()
 
-        if epoch % config.logging.log_interval == 0 or epoch == config.epochs:
-            print(
-                f"Epoch {epoch}/{config.epochs} - Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}"
-            )
+        val_loss = 0
+        val_correct_samples = 0
+        val_num_samples = 0
 
-    print("Training complete.")
+        with torch.no_grad():
+            for x_val, y_val in val_loader:
+                y_predicted = model(x_val)
+
+                val_loss += loss_function(y_predicted, y_val).item() * x_val.size(0)
+
+                predicted_labels = torch.max(y_predicted, 1)[1]
+                val_correct_samples += (y_predicted == y_val).sum().item()
+                val_num_samples += y_val.size(0)
+
+        # Validation loss and accuracy
+        val_avg_loss = val_loss / val_num_samples
+        val_accuracy = val_correct_samples / val_num_samples
+
+        # TODO Log using logger
+        print(
+            f"Epoch {epoch+1}/{epochs} - "
+            f"Train Loss: {train_avg_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, "
+            f"Val Loss: {val_avg_loss:.4f}, Val Accuracy: {val_accuracy:.4f}"
+        )
+
+        # TODO handle wandb logging
+        # if use_wandb:
+        #     wandb.log({
+        #         'epoch': epoch + 1,
+        #         'train_loss': train_avg_loss,
+        #         'train_accuracy': train_accuracy,
+        #         'val_loss': val_avg_loss,
+        #         'val_accuracy': val_accuracy
+        #     })
+
     return model
