f089cc850b54b3b4885ba30ef13574afbcbe2af6
Marcin <mlojek@protonmail.com>
2025-03-19 17:29:39 +0100

    Refactor evaluation script

--- evaluate.py
+++ evaluate.py
@@ -12,8 +12,8 @@ import torch
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader
 
-from .configs.data_model import DatasetConfig, TrainingConfig
-from .models.mlp_classifier import MLPClassifier
+from configs.data_model import DatasetConfig, TrainingConfig
+from models.mlp_classifier import MLPClassifier
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -27,44 +27,41 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    # create a logger
+    # Create a logger
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
     logger = logging.getLogger(__name__)
 
-    # read the dataset config
+    # Read config files
     with open(args.dataset_config, "r", encoding="utf-8") as file_handle:
         dataset_config = DatasetConfig(**json.load(file_handle))
 
-    # read the train dataset and make it a dataloader with one batch
+    with open(args.training_config, "r", encoding="utf-8") as file_handle:
+        training_config = TrainingConfig(**json.load(file_handle))
+
+    # Read test dataset
     with open(
-        dataset_config.save_path / f"{dataset_config.name}.test.pkl", "rb"
+        dataset_config.save_path / f"{dataset_config.name.value}.test.pkl", "rb"
     ) as file_handle:
         test_dataset = pickle.load(file_handle)
         test_loader = DataLoader(test_dataset, batch_size=len(test_dataset))
 
-    # read the training config
-    with open(args.training_config, "r", encoding="utf-8") as file_handle:
-        training_config = TrainingConfig(**json.load(file_handle))
-
-    # read the model
+    # Read model and model state
     model = MLPClassifier(
         input_dim=dataset_config.num_features,
-        hidden_dim=training_config.num_hidden,
+        hidden_dim=dataset_config.num_hidden,
         output_dim=dataset_config.num_classes,
     )
     model.load_state_dict(
         torch.load(
             training_config.save_path
-            / f"{dataset_config.name}.{training_config.optimizer_config.name}.pth",
+            / f"{dataset_config.name.value}.{training_config.optimizer_config.name.value}.pth",
             weights_only=True,
         )
     )
 
-    # evaluate
-    loss_function = CrossEntropyLoss()
-    loss, accuracy = model.evaluate(test_loader, loss_function)
+    # Evaluate the model on test data
+    loss, accuracy = model.evaluate(test_loader, CrossEntropyLoss())
 
-    # log results
-    logger.info(f"Test loss: {loss}, test accuracy: {accuracy}.")
+    logger.info(f"Test loss: {loss:.04f}, test accuracy: {accuracy:.04f}.")
