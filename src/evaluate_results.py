afcc0f87099b80509da99900803bd5d404587baa
mlojek <mlojek@protonmail.com>
2025-05-02 14:31:05 +0200

    read and validate result csv in evaluate_results script

--- src/evaluate_results.py
+++ src/evaluate_results.py
@@ -3,9 +3,9 @@ Script to perform detailed analysis of results.
 """
 
 import argparse
-import glob
 import os
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 from optilab.utils.stat_test import mann_whitney_u_test_grid
@@ -19,6 +19,21 @@ OPTIMIZERS_ORDER = [
     CMAOptimizerName.CMAES.value,
     CMAOptimizerName.LAYERWISE_CMAES.value,
 ]
+
+
+def validate_same_columns(dataframes: List[pd.DataFrame]) -> bool:
+    """
+    Given a list of pandas DataFrames, check if they all share the same column set.
+
+    Args:
+        dataframes (List[pd.DataFrame]): List of dataframes to check.
+
+    Returns:
+        bool: True if all dataframes share the same column set, False otherwise.
+    """
+    all_columns = set().union(*[set(df.columns) for df in dataframes])
+
+    return all([set(df.columns) == all_columns for df in dataframes])
 
 
 if __name__ == "__main__":
@@ -39,12 +54,21 @@ if __name__ == "__main__":
     # sort to desired order
     result_paths = sorted(
         result_paths,
-        key=lambda x: OPTIMIZERS_ORDER.index(x.split(".")[-2]),
+        key=lambda x: OPTIMIZERS_ORDER.index(x.split(".")[1]),
     )
 
-    results = [pd.read_csv(args.input_dir / file_path) for file_path in result_paths]
+    results = [
+        pd.read_csv(args.input_dir / file_path, index_col=0)
+        for file_path in result_paths
+    ]
 
-    # TODO validate if all csvs have the same columns
+    result_names = [file_path.split(".")[1] for file_path in result_paths]
+
+    # validate if all csvs have the same columns
+    assert validate_same_columns(
+        results
+    ), "Not all result files share the same column names!"
+
     # TODO create table with mean values
     # TODO for each column
     # TODO perform stat tests for this column with optilab
